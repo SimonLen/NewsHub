a0cebb8798d1746ac6daa35fff30a55d7f9ff5da
SemyonSorokin <simonlen.gba@gmail.com>
2023-01-29 01:10:50 +0300

    PostSearch view added with filter

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
+from .filters import PostFilter
 
 
 class PostsList(ListView):
@@ -7,9 +8,21 @@ class PostsList(ListView):
     ordering = '-creation_date'
     template_name = 'posts.html'
     context_object_name = 'posts'
+    paginate_by = 1
 
 
 class PostDetail(DetailView):
     model = Post
     template_name = 'post.html'
     context_object_name = 'post'
+
+class PostSearch(ListView):
+    model = Post
+    ordering = '-creation_date'
+    template_name = 'posts_search.html'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        return context
