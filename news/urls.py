85761dd5c9d43e2d56ab4eb982749b821deb3f04
SemyonSorokin <simonlen.gba@gmail.com>
2023-01-30 23:52:27 +0300

    urls structure modified

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PostsList, PostDetail, PostSearch
+from .views import PostsList, PostDetail, PostSearch, PostAddView, PostDeleteView, PostEditView
 
 
 urlpatterns = [
    path('', PostsList.as_view()),
    path('<int:pk>', PostDetail.as_view()),
-   path('search/', PostSearch.as_view())
+   path('search/', PostSearch.as_view()),
+   path('add/', PostAddView.as_view(), name='post_add'),
+   path('delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
+   path('edit/<int:pk>', PostEditView.as_view(), name='post_edit'),
 ]
