2f9dbae2975526ccdea57cfb54c75bd7ff7a9894
SemyonSorokin <simonlen.gba@gmail.com>
2023-02-13 23:16:53 +0300

    Added task for sending emails with weekly digest

---
+++ news/tasks.py
@@ -0,0 +1,30 @@
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+from celery import shared_task
+from datetime import datetime, timedelta
+from .models import Post, Category
+from NewsHub import settings
+
+
+@shared_task
+def new_post_in_category_notifier():
+    today = datetime.now()
+    last_week = today - timedelta(days=7)
+    posts = Post.objects.filter(creation_date__gte=last_week)
+    categories = set(posts.values_list('category__theme', flat=True))
+    subscribers = set(Category.objects.filter(theme__in=categories).values_list('subscribers__email', flat=True))
+    html_content = render_to_string(
+        'daily_post.html',
+        {
+            'link': settings.SITE_URL,
+            'posts': posts,
+        }
+    )
+    msg = EmailMultiAlternatives(
+        subject='Статьи за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers,
+    )
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
