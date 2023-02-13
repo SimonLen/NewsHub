28a2c190e7b2b1fb584333b56c4b0efe3e4abd57
SemyonSorokin <simonlen.gba@gmail.com>
2023-02-14 01:38:27 +0300

    refactored

--- NewsHub/celery.py
+++ NewsHub/celery.py
@@ -7,11 +7,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsHub.settings')
 app = Celery('NewsHub')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks()
-
 app.conf.beat_schedule = {
     'send_weekly_digest_emails': {
         'task': 'news.tasks.new_post_in_category_notifier',
         'schedule': crontab(minute=0, hour=8, day_of_week='monday'),
     },
 }
+
+app.autodiscover_tasks()
