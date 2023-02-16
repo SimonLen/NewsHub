9d6b7a514b976e83da1fe3b9984b9a0f06871d70
SemyonSorokin <simonlen.gba@gmail.com>
2023-02-16 21:15:43 +0300

    Custom command deletenews added

--- news/models.py
+++ news/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Sum
+from django.core.cache import cache
 from .resources import POST_TYPES
 
 
@@ -58,6 +59,10 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/news/{self.id}'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        cache.delete(f'post-{self.pk}')
 
 
 class PostCategory(models.Model):
